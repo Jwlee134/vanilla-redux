adfef24895296c4b5812f9cdc551a501fb4de26c
Jaewon <sorhd134@gmail.com>
2020-11-18 14:47:20 +0900

    11/18

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Detail = () => {};
+const Detail = ({ toDos }) => {
+  return (
+    <div>
+      <h1>{toDos?.text}</h1>
+      <h5>Created at : {toDos?.id}</h5>
+    </div>
+  );
+};
 
-export default Detail;
+const mapStateToProps = (state, ownProps) => {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  return { toDos: state.find((toDo) => toDo.id === parseInt(id)) };
+};
+
+export default connect(mapStateToProps)(Detail);
